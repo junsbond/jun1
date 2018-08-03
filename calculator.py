bd08956f794427dedac956f2a33cd6bea4018671
junsbond <41815879+junsbond@users.noreply.github.com>
2018-08-04 03:38:57 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,32 +1,56 @@
 #!/usr/bin/env python3
 import sys
+takepay = {}
 try:
-    saraly = sys.argv[1:]
+    for arg in sys.argv[1:]:
+        empno, salary = arg.split(":")
+        takepay[empno] = int(salary)
+       # takehomepay(takepay)
 except:
-    print("Params Error")
+    print("Parameter Error")
     sys.exit()
 
-def social():
-    so = saraly*0.165
-    return so
 
-def tax(tax):
-    if salary <= 5000:
-        tax = (salary-social()-3500)*0.30
-    elif salary <=8000:
-        tax = (salary-socila()-3500)*0.10-105
-    elif salary<=12500:
-        tax = (salary-social()-3500)*0.20-555
-    elif salary<=38500:
-        tax = (salary-social()-3500)*0.25-1005
-    elif salary<=58500:
-        tax = (salary-social()-3500)*0.30-2755
-    elif salary<=83500:
-        tax = (salary-social()-3500)*0.35-5505
-    else:
-        tax = (salary-social()-3500)*0.45-13305
-    return('{:.2f}'.format(tax))
+#def social():
+#    so = saraly*0.165
+#    return(so)
 
-def takehomepay(**kw):
-    for arg in sys.argv[1:]:
-    
+#def tax(salary):
+
+def takehomepay(takepay):
+    for empno,salary in takepay.items():
+        so = salary*0.165
+        ntax = salary-so-3500
+        if ntax <= 0:
+            tax = 0
+        elif ntax <= 1500:
+            tax = ntax*0.30
+        elif ntax <= 4500:
+            tax = ntax*0.10-105
+        elif ntax <= 9000:
+            tax = ntax*0.20-555
+        elif ntax <= 35000:
+            tax = ntax*0.25-1005
+        elif nax <= 55000:
+            tax = ntax*0.30-2755
+        elif ntax <= 80000:
+            tax = ntax*0.35-5505
+        else:
+            tax = ntax*0.45-13305
+        thp = salary-so-tax
+        print(empno,end='')
+        print(":",end='')
+        print("{:.2f}".format(thp))
+
+#if __name__ == "__main__":
+
+#    for arg in sys.argv[1:]:
+#        try:
+#            empno,salary = arg.split(":")
+#            takepay[empno] = int(salary)
+           # takehomepay(takepay)
+#        except:
+#            print("Parameter Error")
+#            sys.exit()
+takehomepay(takepay)
+
